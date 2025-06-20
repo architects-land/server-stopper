27aa3c069ba488c7778e2ab42b2bd66cdae8e302
William Hergès <anhgelus@anhgelus.world>
2025-06-20 14:23:46 +0200

    feat(service): cli args

--- service/main.go
+++ service/main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import "flag"
 
+var (
+	useSystemD bool
+	socketPath string
+)
+
+func init() {
+	flag.BoolVar(&useSystemD, "systemd", false, "use systemd")
+	flag.StringVar(&socketPath, "socket", "/run/service-stopped.sock", "path to socket")
+}
+
+func main() {
+	flag.Parse()
 }
