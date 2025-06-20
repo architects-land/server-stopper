a41b52782734c39d304ad83315451f29ddc4e4da
William Hergès <anhgelus@anhgelus.world>
2025-06-20 16:02:58 +0200

    feat(mod): base fabric mod

---
+++ mod/src/main/java/world/anhgelus/architectsland/serverstopper/ServerStopper.java
@@ -0,0 +1,10 @@
+package world.anhgelus.architectsland.serverstopper;
+
+import net.fabricmc.api.ModInitializer;
+
+public class ServerStopper implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
